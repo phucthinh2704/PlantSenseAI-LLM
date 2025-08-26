d5bad749de48420250b32d7a5a9989062701e124
Tran Nguyen Phuc Thinh <thinhphuc2704@gmail.com>
2025-08-26 18:42:30 +0700

    Init base

---
+++ backend/app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+FRONTEND_ORIGIN = os.getenv("FRONTEND_ORIGIN", "http://localhost:5173")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[FRONTEND_ORIGIN],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await fetch("http://127.0.0.1:8000/");
+			const data = await response.json();
+			console.log(data);
+		};
+		fetchData();
+	}, []);
+	return (
+		<>
+			<div>
+				<a
+					href="https://vite.dev"
+					target="_blank">
+					<img
+						src={viteLogo}
+						className="logo"
+						alt="Vite logo"
+					/>
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.jsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+		</>
+	);
+}
+
+export default App;
