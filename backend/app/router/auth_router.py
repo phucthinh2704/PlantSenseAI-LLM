c5ca438a8cc51f20a20970b934a4d2c228c2f2f4
Tran Nguyen Phuc Thinh <thinhphuc2704@gmail.com>
2025-08-28 03:10:41 +0700

    Add deps for auth

--- backend/app/router/auth_router.py
+++ backend/app/router/auth_router.py
@@ -1,21 +1,23 @@
 from app.schema.response_schema import APIResponse
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, Response
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.service import auth_service
-from app.schema.auth_schema import AuthResponse, AuthLogin
+from app.schema.auth_schema import AuthResponse, AuthLogin, TokenRequest
 
-router = APIRouter(
-    prefix="/auth",
-    tags=['Auth']
-)
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
 
 @router.post("/login", response_model=APIResponse)
-async def login(login_data: AuthLogin,
-                 db: Session = Depends(get_db)):
+async def login(login_data: AuthLogin, db: Session = Depends(get_db)):
     result = auth_service.login(login_data, db)
-    return APIResponse(
-        success=True,
-        message="Đăng nhập thành công",
-        data=result
-    )
+    return APIResponse(success=True, message="Đăng nhập thành công", data=result)
+
+
+@router.post("/google")
+async def auth_google(req: TokenRequest):
+    return {
+        "access_token": req.token,
+        "email": "thinh@example.com",
+        "name": "Phuc Thinh",
+    }
--- backend/app/schema/auth_schema.py
+++ backend/app/schema/auth_schema.py
@@ -1,12 +1,18 @@
 from pydantic import BaseModel
 
+
 class AuthLogin(BaseModel):
     email: str
     password: str
 
+
 class AuthResponse(BaseModel):
     id: int
     name: str
+
     class Config:
         from_attributes = True
-    
+
+
+class TokenRequest(BaseModel):
+    token: str
