b1f37fcf81669bd7cae4b567b90ea4b047d80ca5
Tran Nguyen Phuc Thinh <thinhphuc2704@gmail.com>
2025-08-28 03:08:09 +0700

    feat(login): implement login page; integrate Google OAuth v1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,18 @@
-import { useEffect, useState } from "react";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import { Route, Routes } from "react-router-dom";
+import { HomePage, LoginPage } from "./pages";
 function App() {
-	const [count, setCount] = useState(0);
-
-	useEffect(() => {
-		const fetchData = async () => {
-			const response = await fetch(import.meta.env.VITE_API_URL);
-			const data = await response.json();
-			console.log(data);
-		};
-		fetchData();
-	}, []);
 	return (
 		<>
-			<div>
-				<a
-					href="https://vite.dev"
-					target="_blank">
-					<img
-						src={viteLogo}
-						className="logo"
-						alt="Vite logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+			<Routes>
+				<Route
+					path="/"
+					element={<HomePage />}
+				/>
+				<Route
+					path="/login"
+					element={<LoginPage />}
+				/>
+			</Routes>
 		</>
 	);
 }
